4d7e00cbf868fd03385f94a0a8b91b83ae5567f7
Kostiantyn <kostiantyntkachenko@gmail.com>
2021-06-16 19:16:50 +0300

    added database connection and modified filtering

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
+
+	_ "github.com/lib/pq"
 )
+
+var db *sql.DB
 
 // Laptop stores info about laptops in json format
 type Laptop struct {
@@ -18,9 +24,84 @@ type Laptop struct {
 }
 
 func main() {
-	laptops := make([]Laptop, 10)
-	ReadLaptops("laptops.json", &laptops)
-	CoreMenu(laptops)
+	DbConnection()
+	defer db.Close()
+	//laptops := make([]Laptop, 10)
+	//ReadLaptops("laptops.json", &laptops)
+	//featuresMap := getFeatures(laptops)
+	//insertFeaturesDB(featuresMap)
+	//insertLaptopsDB(laptops)
+	//insertLaptopFeaturesDB(laptops)
+	CoreMenu()
+}
+
+// getFeatures assignes features from json as keys in map
+func getFeatures(l []Laptop) map[string]int {
+	var featuresMap = make(map[string]int)
+	for _, lap := range l {
+		for _, feature := range lap.Features {
+			featuresMap[feature] = 0
+		}
+	}
+	return featuresMap
+}
+
+// insertFeaturesDB moves features from json to postgres
+func insertFeaturesDB(featuresMap map[string]int) {
+	query := "INSERT INTO feature(value) VALUES ($1);"
+	for key, _ := range featuresMap {
+		_, err := db.Exec(query, key)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
+// insertLaptopsDB moves laptop data from json to postgres
+func insertLaptopsDB(l []Laptop) {
+	query := "INSERT INTO Laptop(id, name, description, price, sales_price) VALUES ($1, $2, $3, $4, $5);"
+	for _, lapData := range l {
+		_, err := db.Exec(query, lapData.ID, lapData.Name, lapData.Description, lapData.Price, lapData.SalesPrice)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
+// insertLaptopFeaturesDB connects data from two tables into middle one
+func insertLaptopFeaturesDB(laptops []Laptop) {
+	query := "SELECT id FROM feature WHERE value = $1;"
+	query2 := "INSERT INTO laptop_feature(laptop_id, feature_id) VALUES ($1, $2);"
+
+	for _, laptop := range laptops {
+		for _, feature := range laptop.Features {
+			var featureID int
+			row := db.QueryRow(query, feature)
+			err := row.Scan(&featureID)
+			if err != nil {
+				fmt.Println(err)
+			}
+			_, err = db.Exec(query2, laptop.ID, featureID)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+}
+
+// DbConnection Connects to Database
+func DbConnection() {
+	var err error
+	dbconnStr := "user=postgres dbname=postgres password=password host=localhost port=25432 sslmode=disable"
+	db, err = sql.Open("postgres", dbconnStr)
+	if err != nil {
+		panic(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("\nSuccessfully connected to database!\n")
 }
 
 // ReadLaptops reads laptop from file and writes into slice
@@ -42,18 +123,18 @@ func ReadLaptops(namefile string, laptops *[]Laptop) {
 }
 
 // CoreMenu menu for looking through json
-func CoreMenu(l []Laptop) {
+func CoreMenu() {
+
 	var point int
 	for {
 		fmt.Println("1. Read Products")
 		fmt.Println("2. Filter Products")
 		fmt.Println("3. Exit")
-		// Takes users' input
 		fmt.Scan(&point)
 		fmt.Printf("Your choice %d\n", point)
 		switch point {
 		case 1:
-			ReadProducts(l)
+			ReadProducts()
 		case 2:
 			FilterProducts()
 		case 3:
@@ -63,8 +144,22 @@ func CoreMenu(l []Laptop) {
 }
 
 // ReadProducts looks into json for products
-func ReadProducts(l []Laptop) {
-	i := 0
+func ReadProducts() {
+	cmdClear()
+	var (
+		i = 0
+		l = make([]Laptop, 0)
+	)
+	sqlStatement := `SELECT id, name, description, price, sales_price FROM laptop;`
+	rows, err := db.Query(sqlStatement)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for rows.Next() {
+		var laptop Laptop
+		err = rows.Scan(&laptop.ID, &laptop.Name, &laptop.Description, &laptop.Price, &laptop.SalesPrice)
+		l = append(l, laptop)
+	}
 	for {
 		var option int
 		fmt.Printf("Product:\nID_%d\nName: %s\nDescription: %s\nPrice: %d\nSalesPrice: %d\nFeatures: %v\n", l[i].ID, l[i].Name, l[i].Description, l[i].Price, l[i].SalesPrice, l[i].Features)
@@ -72,18 +167,23 @@ func ReadProducts(l []Laptop) {
 		fmt.Println("2. Previous")
 		fmt.Println("3. Edit")
 		fmt.Println("4. Back")
-		_, err := fmt.Scan(&option)
+		_, err = fmt.Scan(&option)
 		if err != nil {
 			fmt.Println(err)
 		}
+
 		switch option {
 		case 1:
+			cmdClear()
 			changeIndex(&i, 1, len(l))
 		case 2:
+			cmdClear()
 			changeIndex(&i, -1, len(l))
 		case 3:
+			cmdClear()
 			EditProduct(&l[i])
 		case 4:
+			cmdClear()
 			return
 		}
 	}
@@ -101,13 +201,15 @@ func changeIndex(i *int, n int, length int) {
 
 // EditProduct edits one product in json
 func EditProduct(l *Laptop) {
+	cmdClear()
+	sqlStatement := `UPDATE laptop SET name = $2, description = $3, price = $4 WHERE id = $1;`
 	for {
 		var option int
 		fmt.Printf("Product:\nID_%d\nName: %s\nDescription: %s\nPrice: %d\nSalesPrice: %d\nFeatures: %v\n", l.ID, l.Name, l.Description, l.Price, l.SalesPrice, l.Features)
 		fmt.Println("1. Change name")
 		fmt.Println("2. Change description")
-		fmt.Println("2. Change price")
-		fmt.Println("3. Back")
+		fmt.Println("3. Change price")
+		fmt.Println("4. Back")
 		_, err := fmt.Scan(&option)
 		if err != nil {
 			fmt.Println(err)
@@ -120,6 +222,10 @@ func EditProduct(l *Laptop) {
 		case 3:
 			fmt.Scan(&l.Price)
 		case 4:
+			_, err = db.Exec(sqlStatement, l.ID, l.Name, l.Description, l.Price)
+			if err != nil {
+				fmt.Println(err)
+			}
 			return
 		}
 	}
@@ -127,7 +233,38 @@ func EditProduct(l *Laptop) {
 
 // FilterProducts filters products in json
 func FilterProducts() {
-	fmt.Println("Filter products works")
+	cmdClear()
+	sqlStatement := "select * from laptop where price between $1 and $2"
+	for {
+		var minPrice int
+		var maxPrice int
+		fmt.Println("Min Price to sort")
+		fmt.Scan(&minPrice)
+
+		fmt.Println("Max Price to sort")
+		fmt.Scan(&maxPrice)
+		if maxPrice < minPrice {
+			fmt.Println("Max price can't be higher then min price")
+			continue
+		}
+		rows, err := db.Query(sqlStatement, minPrice, maxPrice)
+		if err != nil {
+			fmt.Println(err)
+		}
+		for rows.Next() {
+			var laptop Laptop
+			err = rows.Scan(&laptop.ID, &laptop.Name, &laptop.Description, &laptop.Price, &laptop.SalesPrice)
+			fmt.Printf("ID_%d\nName: %s\nDescription: %s\nPrice: %d\nSalesPrice: %d\n", laptop.ID, laptop.Name, laptop.Description, laptop.Price, laptop.SalesPrice)
+		}
+
+		return
+	}
+}
+
+func cmdClear() {
+	cmd := exec.Command("cmd", "/c", "cls")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 }
 
 // Exit exits program
